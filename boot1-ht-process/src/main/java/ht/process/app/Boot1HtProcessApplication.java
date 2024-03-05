a8f23ddba667c625df6ee74d09624e7f342a7f1b
web-tech-76 <webtec76@gmail.com>
2024-03-05 00:17:19 -0500

    added new module ht-processor...

---
+++ boot1-ht-process/src/main/java/ht/process/app/Boot1HtProcessApplication.java
@@ -0,0 +1,13 @@
+package ht.process.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot1HtProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot1HtProcessApplication.class, args);
+    }
+
+}
---
+++ boot1-ht-process/src/test/java/ht/process/app/Boot1HtProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package ht.process.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot1HtProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
